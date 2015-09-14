3b55e6e2ef83ea060070c5ad005048ab3145cfbc
Nelson Ong <contact@nelsonong.com>
2015-09-14 03:06:47 -0400

    Save link values to array when opening extension

--- popup.js
+++ popup.js
@@ -1,5 +1,14 @@
 function onSubmit() {
-
+	
 }
+
+// Save link values.
+var link1 = document.getElementById("link1").value;
+var link2 = document.getElementById("link2").value;
+var link3 = document.getElementById("link3").value;
+var link4 = document.getElementById("link4").value;
+
+// Create link array.
+var linkArray = [link1, link2, link3, link4];
 
 document.getElementById("submit").onclick = onSubmit;
