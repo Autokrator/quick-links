ec82ca81cd95e99ac20eafe5c3fbc2cf9e31ed73
Parth Pathak <ppathak@uwo.ca>
2015-09-13 22:18:05 -0400

    Integrate chrome.commands API

--- background.js
+++ background.js
@@ -1,7 +1,14 @@
-document.addEventListener('keydown', function(event) {
-  if (event.command && String.fromCharCode(event.keyCode) === '1') {
-    console.log("you pressed ctrl-D");
-    event.preventDefault();
-    event.stopPropagation();
-  }
-}, true);
+chrome.commands.onCommand.addListener(function(command) {
+        if(command == "link1"){
+          alert("link1 pressed");
+        }
+        else if(command == "link2"){
+          alert("link2 pressed");
+        }
+        else if(command == "link3"){
+          alert("link3 pressed");
+        }
+        else if(command == "link4"){
+          alert("link4 pressed");
+        }
+      });
