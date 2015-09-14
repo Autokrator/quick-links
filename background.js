096f5c78ef5e1da4a2975b3e22ab763ad07961ff
Nelson Ong <contact@nelsonong.com>
2015-09-14 03:26:08 -0400

    Link commands to links in link array

--- background.js
+++ background.js
@@ -2,19 +2,27 @@ chrome.commands.onCommand.addListener(function(command) {
   //Checks to verify the pressed command
   switch(command){
     case "link1": //command for link1 registered
-      chrome.tabs.create({url: "http://www.youtube.com"});
+		chrome.storage.sync.get(["linkArray"], function(items) {
+			chrome.tabs.create({url: items.linkArray[0]});
+		});
       break;
 
     case "link2": //command for link2 registered
-    chrome.tabs.create({url: "http://www.google.com"});
+		chrome.storage.sync.get(["linkArray"], function(items) {
+			chrome.tabs.create({url: items.linkArray[1]});
+		});
       break;
 
     case "link3": //command for link3 registered
-      chrome.tabs.create({url: "http://www.gmail.com"});
+      chrome.storage.sync.get(["linkArray"], function(items) {
+			chrome.tabs.create({url: items.linkArray[2]});
+		});
       break;
 
     case "link4": //command for link4 registered
-      chrome.tabs.create({url: "http://www.facebook.com"});
+      chrome.storage.sync.get(["linkArray"], function(items) {
+			chrome.tabs.create({url: items.linkArray[3]});
+		});
       break;
 
     default:
