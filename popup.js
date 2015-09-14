bf2ad99140146642bfa110ffded991f4fb1e0f47
Nelson Ong <contact@nelsonong.com>
2015-09-14 03:44:29 -0400

    Only create link variables when button is clicked

--- popup.js
+++ popup.js
@@ -19,11 +19,11 @@ function onLoad() {
 
 function onSubmit() {
 	// Update link array values.
-	link1 = document.getElementById("link1").value;
-	link2 = document.getElementById("link2").value;
-	link3 = document.getElementById("link3").value;
-	link4 = document.getElementById("link4").value;
-	linkArray = [link1, link2, link3, link4];
+	var link1 = document.getElementById("link1").value;
+	var link2 = document.getElementById("link2").value;
+	var link3 = document.getElementById("link3").value;
+	var link4 = document.getElementById("link4").value;
+	var linkArray = [link1, link2, link3, link4];
 	
 	// Store link array in chrome storage API.
 	chrome.storage.sync.set({"linkArray": linkArray}, function() {
@@ -31,15 +31,6 @@ function onSubmit() {
 		message('Settings saved');
 	});
 }
-
-// Save link values.
-var link1 = document.getElementById("link1").value;
-var link2 = document.getElementById("link2").value;
-var link3 = document.getElementById("link3").value;
-var link4 = document.getElementById("link4").value;
-
-// Create link array.
-var linkArray = [link1, link2, link3, link4];
 
 // Load links.
 window.onload = onLoad;
