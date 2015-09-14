789c0b2dac65813a420cbb7b8ac2379690039dbd
Nelson Ong <contact@nelsonong.com>
2015-09-14 05:16:22 -0400

    Fix wrong references to inputs in popup.js

--- popup.js
+++ popup.js
@@ -49,15 +49,15 @@ function onSubmit() {
 		alert("Link 1 has an invalid URL! Please try again.");
 		invalidURL = true;
 	}
-	if ((!ValidUrl(link2))&&(link1)) {
+	if ((!ValidUrl(link2))&&(link2)) {
 		alert("Link 2 has an invalid URL! Please try again.");
 		invalidURL = true;
 	}
-	if ((!ValidUrl(link3))&&(link1)) {
+	if ((!ValidUrl(link3))&&(link3)) {
 		alert("Link 3 has an invalid URL! Please try again.");
 		invalidURL = true;
 	}
-	if ((!ValidUrl(link4))&&(link1)) {
+	if ((!ValidUrl(link4))&&(link4)) {
 		alert("Link 4 has an invalid URL! Please try again.");
 		invalidURL = true;
 	}
