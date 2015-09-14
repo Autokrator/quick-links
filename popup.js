53d1e106265d9d84ada9e8c3e7044040e92837a6
Nelson Ong <contact@nelsonong.com>
2015-09-14 03:20:24 -0400

    Update local link array before updating array on chrome.storage.sync API

--- popup.js
+++ popup.js
@@ -1,6 +1,17 @@
+function onLoad() {
+
+}
+
 function onSubmit() {
+	// Update link array values.
+	link1 = document.getElementById("link1").value;
+	link2 = document.getElementById("link2").value;
+	link3 = document.getElementById("link3").value;
+	link4 = document.getElementById("link4").value;
+	linkArray = [link1, link2, link3, link4];
+	
 	// Store link array in chrome storage API.
-	chrome.storage.sync.set({'linkArray': linkArray}, function() {
+	chrome.storage.sync.set({"linkArray": linkArray}, function() {
 		// Notify that we saved.
 		message('Settings saved');
 	});
@@ -15,4 +26,5 @@ var link4 = document.getElementById("link4").value;
 // Create link array.
 var linkArray = [link1, link2, link3, link4];
 
+window.onload = onLoad;
 document.getElementById("submit").onclick = onSubmit;
