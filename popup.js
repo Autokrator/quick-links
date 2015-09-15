ed3f4967b3ef38b166bb90ee9cdd75dd1a62138a
Parth Pathak <ppathak@uwo.ca>
2015-09-15 13:04:38 -0400

    Debug all features and update UI

--- popup.js
+++ popup.js
@@ -1,157 +1,158 @@
 // Checks if URL is valid or not.
 function ValidUrl(str) {
-  var pattern = new RegExp('(http|ftp|https)://[\\w-]+(\\.[\\w-]+)+([\\w-.,@?^=%&:/~+#-]*[\\w@?^=%&;/~+#-])?');
-  if(!pattern.test(str)) {
-    return false;
-  } else {
-    return true;
-  }
+    // Check if URL is valid.
+    if(str.split(".").length == 2) //case ex: google.com
+        return true;
+    else if(str.split(".").length == 3){ //case ex: https://www.goggle.com | www.google.com
+        var ary = str.split(".");
+        if(ary[0].indexOf("www") != 0){ //check for proper "www" sequence
+            if(ary[0].indexOf("https://www") == 0 || ary[0].indexOf("http://www") == 0) //check http cases
+                return true
+            else
+                return false;
+        }
+        else
+            return true;
+    }
+    else
+        return false;
 }
 
 function onLoad() {
-	// Load link array on chrome.storage.sync API on start.
-	chrome.storage.sync.get(["linkArray"], function(items) {
-		document.getElementById("link1").value = items.linkArray[0];
-	});
-	
-	chrome.storage.sync.get(["linkArray"], function(items) {
-		document.getElementById("link2").value = items.linkArray[1];
-	});
-	
-	chrome.storage.sync.get(["linkArray"], function(items) {
-		document.getElementById("link3").value = items.linkArray[2];
-	});
-	
-	chrome.storage.sync.get(["linkArray"], function(items) {
-		document.getElementById("link4").value = items.linkArray[3];
-	});
-	
-	// Get all commands, and set texts on divs.
+    // Load link array on chrome.storage.sync API on start.
+    chrome.storage.sync.get(["linkArray"], function(items) {
+        document.getElementById("link1").value = items.linkArray[0];
+        document.getElementById("link2").value = items.linkArray[1];
+        document.getElementById("link3").value = items.linkArray[2];
+        document.getElementById("link4").value = items.linkArray[3];
+    });
+
+    // Get all commands, and set texts on divs.
     chrome.commands.getAll(function (commandsArray) {
         for(var i = 1; i < commandsArray.length; i++){
-			
+
             var statusShortcut = document.getElementById("shortcut" + i);
-		
+
             if (commandsArray[i].shortcut == "") {
-				statusShortcut.innerText = "Not Set";
+                statusShortcut.innerText = "Not Set";
                 statusShortcut.style.backgroundColor = "red";
             } else {
-				statusShortcut.innerText = commandsArray[i].shortcut;
+                statusShortcut.innerText = commandsArray[i].shortcut;
                 statusShortcut.style.backgroundColor = "#00E676";
-			}
+            }
         }
     });
-	
-	// Load dark theme if darkTheme = true.
-	chrome.storage.sync.get(["darkTheme"], function(items) {
-		if(items.darkTheme) {
-			var wrapper = document.getElementById("wrapper");
-			wrapper.style.backgroundColor = "#333333";
-			document.body.style.backgroundColor = "#333333";
-			wrapper.style.color = "#FFFFFF";
-			document.getElementById("icon").src="images/icon_48x48_light.png";
-			document.getElementById("darktheme").value = "Light Theme";
-		}
-	});
+
+    // Load dark theme if darkTheme = true.
+    chrome.storage.sync.get(["darkTheme"], function(items) {
+        if(items.darkTheme) {
+            var wrapper = document.getElementById("wrapper");
+            wrapper.style.backgroundColor = "#333333";
+            document.body.style.backgroundColor = "#333333";
+            document.body.style.color = "#FFFFFF";
+            wrapper.style.color = "#FFFFFF";
+            document.getElementById("icon").src="images/icon_48x48_light.png";
+            document.getElementById("link1").style.color = "#FFFFFF";
+            document.getElementById("link2").style.color = "#FFFFFF";
+            document.getElementById("link3").style.color = "#FFFFFF";
+            document.getElementById("link4").style.color = "#FFFFFF";
+        }
+    });
 }
 
 function onSubmit() {
-	// Update link array values.
-	var link1 = document.getElementById("link1").value;
-	var link2 = document.getElementById("link2").value;
-	var link3 = document.getElementById("link3").value;
-	var link4 = document.getElementById("link4").value;
-	
-	// Reject save if URLs are not valid.
-	var invalidURL = false;
-	if ((!ValidUrl(link1))&&(link1)) {
-		alert("Link 1 has an invalid URL! Please try again.");
-		invalidURL = true;
-	}
-	if ((!ValidUrl(link2))&&(link2)) {
-		alert("Link 2 has an invalid URL! Please try again.");
-		invalidURL = true;
-	}
-	if ((!ValidUrl(link3))&&(link3)) {
-		alert("Link 3 has an invalid URL! Please try again.");
-		invalidURL = true;
-	}
-	if ((!ValidUrl(link4))&&(link4)) {
-		alert("Link 4 has an invalid URL! Please try again.");
-		invalidURL = true;
-	}
-	if (invalidURL)
-		return;
-	
-	var linkArray = [link1, link2, link3, link4];
-	
-	// Store link array in chrome storage API.
-	chrome.storage.sync.set({"linkArray": linkArray}, function() {
-		// Notify that we saved.
-		message('Settings saved');
-	});
-	
-	document.getElementById("notification").style.display = "none";
-	document.getElementById("notification").style.display = "block";
+    // Reject save if URLs are not valid.
+    var invalidURL = false;
+    for(i = 1; i < 5; i++){
+        if((!ValidUrl(document.getElementById("link" + i).value))
+                    &&(document.getElementById("link" + i).value)){
+            document.getElementById("link" + i).className = "invalidInput";
+            invalidURL = true;
+        }
+    }
+    if (invalidURL)
+    return;
+
+    //update the link array
+    var linkArray = [document.getElementById("link1").value,
+                     document.getElementById("link2").value,
+                     document.getElementById("link3").value,
+                     document.getElementById("link4").value];
+
+    // Store link array in chrome storage API.
+    chrome.storage.sync.set({"linkArray": linkArray}, function(){
+        document.getElementById("notification").style.display = "none";
+        document.getElementById("notification").style.display = "block";
+    });
 }
 
 function toggleDarkTheme() {
-	// Change GUI theme.
-	chrome.storage.sync.get(["darkTheme"], function(items) {
-		// Grab wrapper from HTML.
-		var wrapper = document.getElementById("wrapper");
-		
-		// Toggle GUI colors.
-		if(items.darkTheme) {
-			wrapper.style.backgroundColor = "#FFFFFF";
-			document.body.style.backgroundColor = "#FFFFFF";
-			wrapper.style.color = "#000000";
-			var icon = document.getElementById("icon").src="images/icon_48x48.png";
-		} else {
-			wrapper.style.backgroundColor = "#333333";
-			document.body.style.backgroundColor = "#333333";
-			wrapper.style.color = "#FFFFFF";
-			var icon = document.getElementById("icon").src="images/icon_48x48_light.png";
-		}
-		
-		// Save new dark theme settings.
-		chrome.storage.sync.set({"darkTheme": !items.darkTheme}, function() {
-			// Notify that we saved.
-			message('Settings saved');
-		});
-	});
+    // Change GUI theme.
+    chrome.storage.sync.get(["darkTheme"], function(items) {
+        // Grab wrapper from HTML.
+        var wrapper = document.getElementById("wrapper");
+
+        // Toggle GUI colors.
+        if(items.darkTheme) {
+            wrapper.style.backgroundColor = "#F5F5F5";
+            document.body.style.backgroundColor = "#F5F5F5";
+            wrapper.style.color = "#000000";
+            document.getElementById("link1").style.color = "#000000";
+            document.getElementById("link2").style.color = "#000000";
+            document.getElementById("link3").style.color = "#000000";
+            document.getElementById("link4").style.color = "#000000";
+            var icon = document.getElementById("icon").src="images/icon_48x48.png";
+        } else {
+            wrapper.style.backgroundColor = "#333333";
+            document.body.style.backgroundColor = "#333333";
+            wrapper.style.color = "#FFFFFF";
+            document.getElementById("link1").style.color = "#FFFFFF";
+            document.getElementById("link2").style.color = "#FFFFFF";
+            document.getElementById("link3").style.color = "#FFFFFF";
+            document.getElementById("link4").style.color = "#FFFFFF";
+            var icon = document.getElementById("icon").src="images/icon_48x48_light.png";
+        }
+
+        // Save new dark theme settings.
+        chrome.storage.sync.set({"darkTheme": !items.darkTheme}, function() {});
+    });
 }
 
 function onStatusClick() {
-	chrome.tabs.create({url: "chrome://extensions/configureCommands"});
+    chrome.tabs.create({url: "chrome://extensions/configureCommands"});
 }
 
-function onMouseHover() {	
-	var statusElement = this;
-	var current = parseInt(this.id.slice(-1));
-	// Check statuses if shortcuts are set, and change colors accordingly on hover.
-	chrome.commands.getAll(function (commandsArray) {
-		
-		if (commandsArray[current].shortcut == "") {
-			statusElement.style.backgroundColor = "#C62828";
-		} else {
-			statusElement.style.backgroundColor = "#00C853";
-		}
+function onMouseHover() {
+    var statusElement = this;
+    var current = parseInt(this.id.slice(-1));
+    // Check statuses if shortcuts are set, and change colors accordingly on hover.
+    chrome.commands.getAll(function (commandsArray) {
+        //Checking to see if shortcut is set
+        if (commandsArray[current].shortcut == "") {
+            statusElement.style.backgroundColor = "#C62828";
+        } else {
+            statusElement.style.backgroundColor = "#00C853";
+        }
     });
 }
 
 function onMouseOut() {
-	var statusElement = this;
-	var current = parseInt(this.id.slice(-1));
-	// Check statuses if shortcuts are set, and change colors accordingly on hover.
-	chrome.commands.getAll(function (commandsArray) {
-		
-		if (commandsArray[current].shortcut == "") {
-			statusElement.style.backgroundColor = "red";
-		} else {
-			statusElement.style.backgroundColor = "#00E676";
-		}
+    var statusElement = this;
+    var current = parseInt(this.id.slice(-1));
+    // Check statuses if shortcuts are set, and change colors accordingly on hover.
+    chrome.commands.getAll(function (commandsArray) {
+
+        if (commandsArray[current].shortcut == "") {
+            statusElement.style.backgroundColor = "red";
+        } else {
+            statusElement.style.backgroundColor = "#00E676";
+        }
     });
+}
+
+function onInputFocus(){
+    // resets style to default in case it was previously invalid
+    this.className = "input";
 }
 
 // Load links.
@@ -179,4 +180,10 @@ document.getElementById("shortcut3").onmouseout = onMouseOut;
 document.getElementById("shortcut4").onmouseout = onMouseOut;
 
 // Clicking icon toggles dark theme
-document.getElementById("icon").onclick = toggleDarkTheme;
+document.getElementById("cf").onclick = toggleDarkTheme;
+
+// Input focus resets input style
+document.getElementById("link1").onfocus = onInputFocus;
+document.getElementById("link2").onfocus = onInputFocus;
+document.getElementById("link3").onfocus = onInputFocus;
+document.getElementById("link4").onfocus = onInputFocus;
